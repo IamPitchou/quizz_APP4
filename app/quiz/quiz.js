c1a143406cad86efd62fab3123ae7b3fff503fdf
sebferrer <sebastien@sebferrer.fr>
2015-05-19 23:11:50 +0200

    Quiz > Game : Réponses mélangées + détection des bonnes réponses
    Enfin...

--- app/quiz/quiz.js
+++ app/quiz/quiz.js
@@ -22,13 +22,7 @@ angular.module('coq')
 			$scope.duel = response;
 			$scope.currentQuestion = $scope.duel.round.collection.questions[$scope.numCurrentQuestion];
 
-			$scope.answers[0] = $scope.currentQuestion.answerOK;
-			$scope.answers[1] = $scope.currentQuestion.answer1;
-			$scope.answers[2] = $scope.currentQuestion.answer2;
-			$scope.answers[3] = $scope.currentQuestion.answer3;
-			// Shuffle
-			for(var j, x, i = 4; i; j = Math.floor(Math.random() * i), x = $scope.answers[--i], $scope.answers[i] = $scope.answers[j], $scope.answers[j] = x);
-
+			shuffleAnswers();
 		});
 
 		var url = "app/json/getScoreTotalDuel.php";
@@ -36,18 +30,32 @@ angular.module('coq')
 			$scope.scoreTotalDuel = response;
 		});
 
-		var url = "app/php/getShuffledAnswers.php?id="+$scope.numCurrentQuestion;
-		$http.get(url).success( function(response) {
-			$scope.shuffledAnswersTab = $sce.trustAsHtml(response);
-		});
+		function shuffleAnswers() {
+			$scope.answers[0] = Array(1, $scope.currentQuestion.answerOK);
+			$scope.answers[1] = Array(0, $scope.currentQuestion.answer1);
+			$scope.answers[2] = Array(0, $scope.currentQuestion.answer2);
+			$scope.answers[3] = Array(0, $scope.currentQuestion.answer3);
+			for(var j, x, i = 4; i; j = Math.floor(Math.random() * i), x = $scope.answers[--i], $scope.answers[i] = $scope.answers[j], $scope.answers[j] = x);
+			$scope.answersShuffled = [
+		        {isOk : $scope.answers[0][0], name : $scope.answers[0][1]},
+		        {isOk : $scope.answers[1][0], name : $scope.answers[1][1]},
+		        {isOk : $scope.answers[2][0], name : $scope.answers[2][1]},
+		        {isOk : $scope.answers[3][0], name : $scope.answers[3][1]},
+		    ];
+		}
 
 		$scope.valider = function(isOk) {
+			if(isOk) {
+				$scope.score = $scope.score+1;
+			}
 			if($scope.numCurrentQuestion < 4) {
-				if(isOk) {
-					$scope.score = $scope.score+1;
-				}
 	            $scope.numCurrentQuestion = $scope.numCurrentQuestion+1;
 	            $scope.currentQuestion = $scope.duel.round.collection.questions[$scope.numCurrentQuestion];
+
+	            shuffleAnswers(); 
+            }
+            else if($scope.numCurrentQuestion >= 4) {
+            	$scope.numCurrentQuestion = 0;
             }
         }
 
