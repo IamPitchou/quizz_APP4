7cdff09f51bbae271ff8a2251498d18ea2f06262
Tim <timothee.avenard@gmail.com>
2015-05-20 11:20:01 +0200

    Présentation utilisateurs avec lesquels on a des duels

--- app/json/getAllDuelsOfUser.php
+++ app/json/getAllDuelsOfUser.php
@@ -1,16 +1,24 @@
 {
     "users": [
         {
-            "name": "User 1"
+            "name": "User 1",
+            "score1": 3,
+            "score2": 5
         },
         {
-            "name": "User 2"
+            "name": "User 2",
+            "score1": 3,
+            "score2": 5
         },
         {
-            "name": "User 3"
+            "name": "User 3",
+            "score1": 3,
+            "score2": 5
         },
         {
-            "name": "User 4"
+            "name": "User 4",
+            "score1": 3,
+            "score2": 5
         }
     ]
 }
