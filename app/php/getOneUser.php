6205bb02173ae34b59d041602d96e59f22b3fa54
aldoromano <tiano.roma@gmail.com>
2015-05-21 17:12:43 +0200

    Register.html + Accès utilisateur unique

---
+++ app/php/getOneUser.php
@@ -0,0 +1,31 @@
+<?php
+
+	include_once("../model/coq_user.php");
+
+    $user_id = htmlspecialchars($_GET["user"]);
+    if (!checkVar($user_id))
+        echo ('Error unable to find the user');
+    else
+    {
+        $user = new coq_user();
+        $data = $user->find($user_id);
+        if ($data == 0)
+            echo("Error id user invalid");
+        else
+        {
+
+            $user->init
+            (
+                data['login'],
+                data['pwd'],
+                data['pseudo'],
+                data['rights']
+            );
+
+            echo $user->JSON();
+        }
+    }
+
+
+?>
+
