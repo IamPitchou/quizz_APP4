47bd7d32a6ddba47a1212b41dfe395945898e83c
Lowinf <johan.douillard@gmail.com>
2015-05-21 14:46:58 +0200

    getAllUsers implémentée

---
+++ app/php/getAllUsers.php
@@ -0,0 +1,22 @@
+<?php
+	include_once("../model/coq_user.php");
+	$user_id = htmlspecialchars($_GET["user"]);
+    if (!checkVar($user_id))
+        echo ('Error unable to find the duel');
+    else
+    {
+        $user = new coq_user();
+        $data = $user->get_AllUsers($user_id);
+        if ($data == 0)
+            echo("Error id user invalid");
+        else
+        {
+            $arr = array();
+            $arr_data_user = array();
+            foreach ($data as $d)
+                $arr_data_user[] = array("id" => $d["id"], "pseudo" => $d["pseudo"]);
+            $arr = array("users" => $arr_data_user);
+            json_encode($arr);
+        }
+    }
+?>
