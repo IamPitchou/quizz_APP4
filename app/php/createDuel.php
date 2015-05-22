3ff8f89db4931500ec45e06b7bb66e2e0341d45f
sebferrer <sebastien@sebferrer.fr>
2015-05-22 10:03:15 +0200

    Quiz terminé
    On peut maintenant déclarer des duels et jouer complètement

--- app/php/createDuel.php
+++ app/php/createDuel.php
@@ -12,15 +12,27 @@
         else
         {
             $duel = new coq_duel();
-            $round = new coq_round();
-            $round->init(1, 1, 1, 1, 0, 0, 0, 0);
-            $round->add();
-            $id_round = $round->get_last_round_created();
-            if ($id_round < 1)
+
+            $round1 = new coq_round();
+            $round1->init(1, 1, 1, 1, 0, 0, 0, 0);
+            $round1->add();
+            $id_round1 = $round1->get_last_round_created();
+
+            $round2 = new coq_round();
+            $round2->init(2, 2, 2, 2, 0, 0, 0, 0);
+            $round2->add();
+            $id_round2 = $round2->get_last_round_created();
+
+            $round3 = new coq_round();
+            $round3->init(3, 3, 3, 3, 0, 0, 0, 0);
+            $round3->add();
+            $id_round3 = $round3->get_last_round_created();
+
+            if ($id_round1 < 1)
                 echo ("Unable to find the last round id");
             else
             {
-                $duel->init($user1_id, $user2_id, $id_round,  1, 0, 0);
+                $duel->init($user1_id, $user2_id, $id_round1,  1, 0, 0);
                 $duel->add();
                 $id_duel = $duel->get_last_duel_created();
                 if ($id_duel < 1)
